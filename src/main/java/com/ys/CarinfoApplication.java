e9e4f843a8321b1309d0f7c0797396aa1fe5ccb4
youngseok <dblys152@gmail.com>
2021-09-26 15:49:16 +0900

    ..

---
+++ src/main/java/com/ys/CarinfoApplication.java
@@ -0,0 +1,13 @@
+package com.ys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarinfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ys/CarinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
