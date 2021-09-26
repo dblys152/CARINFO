9ffc5ccf2a11243790be02b1a3040429df045ba2
youngseok <dblys152@gmail.com>
2021-09-26 15:39:07 +0900

    프로젝트 업로드

---
+++ CARINFO/src/main/java/com/ys/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.ys.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@RequestMapping(value="")
+	public String test() {
+		
+		return "NewFile";
+	}
+}
