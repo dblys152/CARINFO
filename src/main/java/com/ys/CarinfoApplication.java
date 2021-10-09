b3fc8e731798494353c792b06ebb2f5a80739168
youngseok <dblys152@gmail.com>
2021-10-09 18:44:15 +0900

    tomcat StandardJarScanner 비활성화

--- src/main/java/com/ys/CarinfoApplication.java
+++ src/main/java/com/ys/CarinfoApplication.java
@@ -1,7 +1,11 @@
 package com.ys;
 
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,6 +16,16 @@ public class CarinfoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarinfoApplication.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatFactory() {
+		return new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+			}
+		};
 	}
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
